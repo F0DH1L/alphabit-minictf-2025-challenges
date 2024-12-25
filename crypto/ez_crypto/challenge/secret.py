6b8887259120aede30fa88566fcbf31e8e252111
Yaniss <y.aitouali@esi-sba.dz>
2024-12-25 05:18:10 +0100

    crypto chall

---
+++ crypto/ez_crypto/challenge/secret.py
@@ -0,0 +1,7 @@
+FLAG="Alphabit{D0_p30pl3_st1ll_us3_th1s_k1nd_0f_3ncrypt10ns?}"
+def encrypt(pt):
+    ct = ''
+    for i in range(len(pt)-1):
+        ct+=chr(ord(pt[i])^ord(pt[i+1]))
+    return ct.encode().hex()
+
