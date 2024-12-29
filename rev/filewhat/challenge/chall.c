0ffa8968f087b756f656069ef8dab5faeee8df5a
Joey <y8w3ly@h4ck3r.esi-sba.dz>
2024-12-30 00:07:20 +0100

    updated filewhat chall

--- rev/filewhat/challenge/chall.c
+++ rev/filewhat/challenge/chall.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h> 
+
 char buf[32];
-int main(int argc, char* argv[], char* envp[]){
-        if(argc<2){
-                printf("Give me a number \n");
-                return 0;
-        }
-        int fd = atoi( argv[1] )*5 ;
+int main(){
+        int x;
+        printf("Give me a number : ");
+        scanf("%d",&x) ;
+        int fd = (x*5)%2 ;
         int len = read(fd, buf, 32);
-        if(!strcmp("ALPHABIT\n", buf)){
+        if(!strcmp("ALPHABIT", buf)){
                 system("/bin/cat flag.txt");
                 exit(0);
         }
