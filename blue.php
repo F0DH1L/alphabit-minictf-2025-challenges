fc362effde81b4e7e3bd7c7b4c43cf1bdd4e33d9
ouby <ouby@s>
2025-01-21 20:02:13 +0100

    first challenge LFI by  s443.x

---
+++ blue.php
@@ -0,0 +1,4 @@
+<?php 
+echo "<pre>maybe the flag on the red one trust me !!!</pre>" ;
+echo "sir I tell you the truth , there are some sensitive files on this directory <code>etc/passwd </code> ";
+?>
