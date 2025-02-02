cfaf789ff090268cbca6e11728e3736da3e96b89
fodhil-ben <benhibafodhil@gmail.com>
2025-02-02 10:53:20 +0000

    edited yml files

--- scripts/wave.sh
+++ scripts/wave.sh
@@ -1,39 +1,23 @@
 #!/bin/bash
 
 # List of paths to Docker Compose files for list 1
-compose_paths_2=(
-  "./web/SQLi_1/docker-compose.yml"
-  "./web/SQLi_2/docker-compose.yml"
-  "./web/SQLi_3/docker-compose.yml"
-  "./web/SQLi_4/docker-compose.yml"
-  "./web/login/docker-compose.yml"
-  "./misc/Secret/docker-compose.yml"
-  "./misc/yaml/docker-compose.yml"
-  "./pwn/echo/docker-compose.yml"
-
-)
 
 compose_paths_1=(
-  "./web/bigHEAD/docker-compose.yml"
-  "./web/fREe/docker-compose.yml"
-  "./web/weird/docker-compose.yml"
-  "./web/Inspector/docker-compose.yml"
-  "./web/PingPong1/docker-compose.yml"
-  "./web/PingPong2/docker-compose.yml"
-  "./misc/matrix/docker-compose.yml"
-  "./misc/Path/docker-compose.yml"
-  "./misc/Autobash/docker-compose.yml"
-  "./pwn/Crash/docker-compose.yml"
-  "./pwn/DeadBeef/docker-compose.yml"
-  "./pwn/ret2win/docker-compose.yml"
+  "./web/forgery/docker-compose.yml"
+  "./web/lfi/docker-compose.yml"
+  "./web/sequel/docker-compose.yml"
+  "./web/injector/docker-compose.yml"
+  "./pwn/oops/docker-compose.yml"
+  "./pwn/ret2ret/docker-compose.yml"
+  "./pwn/ret2exit/docker-compose.yml"
+  "./pwn/shellcoder/docker-compose.yml"
+)
+
+
+compose_paths_2=(
 )
 
 compose_paths_3=(
-  "./web/blogger/docker-compose.yml"
-  "./misc/py1/docker-compose.yml"
-  "./misc/py2/docker-compose.yml"
-  "./misc/Guess/docker-compose.yml"
-  "./crypto/flagger/docker-compose.yml"
 )
 # Check if an integer argument was passed
 if [[ ! $1 =~ ^[0-9]+$ ]]; then
