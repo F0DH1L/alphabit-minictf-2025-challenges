b6e1c8a2a1e66998b331b5f7534e5a759438eef2
fodhil-ben <benhibafodhil@gmail.com>
2025-02-04 07:53:50 +0000

    edited wave2 challs

--- scripts/wave.sh
+++ scripts/wave.sh
@@ -15,9 +15,28 @@ compose_paths_1=(
 
 
 compose_paths_2=(
+  "./misc/LinearReg/docker-compose.yml"
+
+  "./web/lfi/docker-compose.yml"
+  "./web/sequel/docker-compose.yml"
+  "./web/injector/docker-compose.yml"
+
+  "./crypto/ez_crypto/docker-compose.yml"
+  "./crypto/rsa1/docker-compose.yml"
+  "./crypto/rsa2/docker-compose.yml"
+
+  "./pwn/bank/docker-compose.yml"
+  "./pwn/pp/docker-compose.yml"
+  "./pwn/ppp/docker-compose.yml"
+  "./pwn/shellcoder-revenge/docker-compose.yml"
+
+  "./reverse/patchme/docker-compose.yml"
+
 )
 
 compose_paths_3=(
+  "./crypto/rsa3/docker-compose.yml"
+  "./crypto/rsa4/docker-compose.yml"
 )
 # Check if an integer argument was passed
 if [[ ! $1 =~ ^[0-9]+$ ]]; then
