1e60ae17f407756fc1abb36589002b658a2cc802
Yaniss <146975315+y8w3ly@users.noreply.github.com>
2025-02-12 21:47:07 +0100

    Update solve.py

--- crypto/ez_crypto/solution/solve.py
+++ crypto/ez_crypto/solution/solve.py
@@ -1,19 +1,20 @@
 from pwn import process,remote
 import string
-#r = remote('host',port)
+#r = remote('0.0.0.0',1337)
 p = process("../challenge/chall.py")
 
 p.recvuntil(b'> ')
 p.sendline(b'1')
 enc = p.recvline()
-inp = b'A'
+inp = b'Alphabit{'
 chars = string.ascii_letters + string.digits + '{}_?!'
 for j in range(len(enc.decode())//2):
     for i in chars:
         p.sendlineafter(b'> ',b'2')
         p.sendlineafter(b'What do you want to encrypt ? ',inp + i.encode())
         res = p.recvline()
-        if (res[:len(res)-1] == enc[:len(inp)*2]):
+        if (enc.decoce().startswith(res.decode()):
             inp += i.encode()
             break
-print(inp)
+
+open("flag.txt","w").write(inp.decode())
